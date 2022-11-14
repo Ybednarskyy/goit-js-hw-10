2296f38d83745228329de38747baaaa4fa57cd5f
Ybednarskyy <y.bednarskyy@gmail.com>
2022-11-14 21:34:51 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const searchBox = document.querySelector('#search-box');
+
+function fetchCountries(name) {
+  fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(country => {
+      console.log(country);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+searchBox.addEventListener('input', () => {
+  fetchCountries(searchBox.value);
+});
